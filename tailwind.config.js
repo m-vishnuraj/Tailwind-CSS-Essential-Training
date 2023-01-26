f01c40cf41c4c3062f6a449285883f024fbf316d
m-vishnuraj <mvishnurajm@gmail.com>
2023-01-27 01:13:32 +0530

    color property completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // vishnu: "#572143",
+        vishnu: {
+          50: "#f9f0f5",
+          100: "#f2e0eb",
+          200: "#e4c1d6",
+          300: "#d6a2c1",
+          400: "#b96aa0",
+          500: "#9a3f7a",
+          600: "#8a376f",
+          700: "#6a2a55",
+          800: "#4c1d3b",
+        },
+      },
+    },
   },
   plugins: [],
 };
