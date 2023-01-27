0a19e6627e88dea021a1bf326352f731270f2a13
m-vishnuraj <mvishnurajm@gmail.com>
2023-01-27 12:19:21 +0530

    padding

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 function App() {
-  return <div>
-    <h1 className="text-9xl subpixel-antialiased ">Title 1</h1>
-    <h2 className="text-2xl antialiased">Title 2</h2>
-    <h3 className="text-xl">Title 3</h3>
-    <p className="text-base underline decoration-red-400 decoration-wavy decoration-2 underline-offset-8 leading-loose capitalize">A regular paragraph</p>
-    <p className="text-sm">A description paragraph</p>
-    <p className="text-xs note">A little note</p>
-    <p className="">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere blanditiis sit iste vel dicta repellat eius delectus distinctio reprehenderit, nisi numquam adipisci totam sint aspernatur ut exercitationem, corrupti alias ad!</p>
+  return <div className="bg-red-400 text-white font-bold p-4">
+    hello
   </div>;
 }
 
