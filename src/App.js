2455b2c3e93ab2fd2c7f420bb7e092914729145d
m-vishnuraj <mvishnurajm@gmail.com>
2023-01-27 20:45:55 +0530

    margin

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 function App() {
-  return <div className="bg-red-400 text-white font-bold p-4">
-    hello
+  return <div>
+    <div className="bg-red-400 text-white font-bold p-4">hello</div>
+    <div className="bg-blue-400 text-white font-bold m-4">hello</div>
+
+    <div>
+      <div className="h-16 bg-blue-600 w-36 opacity-20"></div>
+      <div className="bg-red-300 -mt-8">-mt8</div>
+    </div>
   </div>;
 }
 
