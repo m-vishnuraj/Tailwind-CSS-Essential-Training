e6e508b1622a834627f3a8b69f4bc708b94bb02c
m-vishnuraj <mvishnurajm@gmail.com>
2023-01-27 07:30:04 +0530

    typography

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 function App() {
   return <div>
-    <h1 className="text-9xl">Title 1</h1>
-    <h2 className="text-2xl">Title 2</h2>
+    <h1 className="text-9xl subpixel-antialiased ">Title 1</h1>
+    <h2 className="text-2xl antialiased">Title 2</h2>
     <h3 className="text-xl">Title 3</h3>
-    <p className="text-base">A regular paragraph</p>
+    <p className="text-base underline decoration-red-400 decoration-wavy decoration-2 underline-offset-8 leading-loose capitalize">A regular paragraph</p>
     <p className="text-sm">A description paragraph</p>
     <p className="text-xs note">A little note</p>
+    <p className="">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere blanditiis sit iste vel dicta repellat eius delectus distinctio reprehenderit, nisi numquam adipisci totam sint aspernatur ut exercitationem, corrupti alias ad!</p>
   </div>;
 }
 
